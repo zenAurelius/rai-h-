955333f8485042dff70788da206b10baec6c2fc4
zenAurelius <loic2845@gmail.com>
2024-06-24 08:15:38 +0200

    Add files via upload

--- evaluate.py
+++ evaluate.py
@@ -1,51 +1,67 @@
 
 import pandas as pd
 import time
+import numpy as np
 
 import combine
 import elo1
+import skills
 
 
 
 def prepare(df):
     start_time = time.time()
-    #df = combine.clean(df)
-    #df = combine.un_vs_un(df)
-    #df = elo1.calc_elo(df, 'ch_nom')
-    df = elo1.calc_elo(df, 'ch_driver')
+    df = combine.clean(df)
     elapsed_time = time.time() - start_time
     print(f"Elapsed time: {elapsed_time:.2f} seconds")
+    
+    start_time = time.time()
+    df = combine.un_vs_un(df)
+    elapsed_time = time.time() - start_time
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
+    #df = elo1.calc_elo(df, 'ch_nom')
+    #df = elo1.calc_elo(df, 'ch_driver')
+    
+    start_time = time.time()
+    df = skills.calc_oskill(df)
+    elapsed_time = time.time() - start_time
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
     return df
 
 def pct_place(df):
     n = len(df)
     np = len(df[df.ch_estplace_1])
-    ng = len(df[df.ch_estgagant_1])
+    ng = len(df[df.ch_estgagnant_1])
     print(f'placé {100 * np / n}%  [{np}/{n}] - gagnant {100 * ng / n}% [{ng}/{n}]')
 
 
-df = pd.read_csv('./data/2015cce2.csv')
-#df = prepare(df)
-#df.to_csv('./data/2015cce2.csv')
+'''df = pd.read_csv('./data/pmu2016.csv')
+df = prepare(df)
+df.to_csv('./data/pmu2016cc_os.csv', index=False)'''
+
+
 
 ##df[df.ch_nom_1 == 'VALEUR DANOVER'].to_csv('test.csv')
 
-
-'''result = result = df[df.ch_dernierRapportDirect_rapport_1 > 0].groupby(['aid_cr', 'ch_nom_1']).first().reset_index()
+df = pd.read_csv('./data/pmu2016cc_os.csv')
+result = result = df[df.ch_dernierRapportDirect_rapport_1 > 0].groupby(['aid_cr', 'ch_nom_1']).first().reset_index()
+result['ch_estplace_1'] = (result['ch_ordreArrivee_1'] < 3) | ((result['ch_ordreArrivee_1'] == 3) & (result['cr_nombreDeclaresPartants'] > 7))
 print(len(result))
-result['ELOC_R'] = result.groupby('aid_cr')['ELO_ch_nom_1'].rank(ascending=False)
-result['ELOD_R'] = result.groupby('aid_cr')['ELO_ch_driver_1'].rank(ascending=False)
+result['ELOC_R'] = result.groupby('aid_cr')['OS_MU_CH1'].rank(ascending=False)
+result['ELOD_R'] = result.groupby('aid_cr')['OS_MU_DV1'].rank(ascending=False)
 result['COTE_R'] = result.groupby('aid_cr')['ch_dernierRapportDirect_rapport_1'].rank()
+#result.to_csv('./data/ranked.csv', index=False)
 
-pct_place(result[result.ELOD_R == 1.0])
-pct_place(result[result.ELOD_R == 2.0])
-pct_place(result[result.ELOD_R == 3.0])
-pct_place(result[result.ELOD_R == 10.0])
+pct_place(result[result.ELOC_R == 1.0])
+pct_place(result[result.ELOC_R == 2.0])
+pct_place(result[result.ELOC_R == 3.0])
+pct_place(result[result.ELOC_R == 10.0])
 print('--')
-pct_place(result[(result.ELOD_R == 1.0) & (result.ELOC_R == 1.0)])
+'''pct_place(result[(result.ELOD_R == 1.0) & (result.ELOC_R == 1.0)])
 pct_place(result[(result.ELOD_R == 2.0) & (result.ELOC_R == 2.0)])
 pct_place(result[(result.ELOD_R == 3.0) & (result.ELOC_R == 3.0)])
-pct_place(result[(result.ELOD_R == 10.0) & (result.ELOC_R == 10.0)])
+pct_place(result[(result.ELOD_R == 10.0) & (result.ELOC_R == 10.0)])'''
 print('--')
 pct_place(result[result.COTE_R == 1.0])
 pct_place(result[result.COTE_R == 2.0])
@@ -57,6 +73,7 @@ pct_place(result[(result.COTE_R == 2.0) & (result.ELOC_R == 1.0)])
 pct_place(result[(result.COTE_R == 3.0) & (result.ELOC_R == 1.0)])
 pct_place(result[(result.COTE_R == 10.0) & (result.ELOC_R == 1.0)])
 print('--')
+'''
 pct_place(result[(result.COTE_R == 1.0) & (result.ELOD_R == 1.0)])
 pct_place(result[(result.COTE_R == 2.0) & (result.ELOD_R == 1.0)])
 pct_place(result[(result.COTE_R == 3.0) & (result.ELOD_R == 1.0)])
--- skills.py
+++ skills.py
@@ -5,76 +5,78 @@ import numpy as np
 df =df[['aid_cr', 'ch_nom_1', 'ch_driver_1', 'ch_ordreArrivee_1', 'ch_nom_2', 'ch_driver_2', 'ch_ordreArrivee_2']]
 df.to_csv('./data/volcania_s.csv', index=False)'''
 
-df = pd.read_csv('./data/volcania_s.csv')
-df['win'] = (np.sign(df.ch_ordreArrivee_2 - df.ch_ordreArrivee_1) + 1.0) / 2.0
-print(df.describe())
 
-params= {'dsg':float(25/3), 'dmu':float(25), 'tau':float(25.0 / 300.0), 'beta':float(25.0 / 6.0)}
+
+'''params= {'dsg':float(25/3), 'dmu':float(25), 'tau':float(25.0 / 300.0), 'beta':float(25.0 / 6.0)}
 b_sg = {}
-b_mu = {}
+b_mu = {}'''
 
 
-dsg = params.get('dsg')
-dmu = params.get('dmu')
-tsq = params.get('tau') ** 2
-for i in range(1, 3):
-    # init sigma / mu -ok
-    df[f'S_SG_CH{i}'] = df[f'ch_nom_{i}'].astype(str).map(b_sg).fillna(dsg)
-    df[f'S_SG_DV{i}'] = df[f'ch_driver_{i}'].astype(str).map(b_sg).fillna(dsg) 
-    df[f'S_MU_CH{i}'] = df[f'ch_nom_{i}'].astype(str).map(b_mu).fillna(dmu)
-    df[f'S_MU_DV{i}'] = df[f'ch_driver_{i}'].astype(str).map(b_mu).fillna(dmu) 
-
-    # Correct Sigma With Tau -ok
-    df[f'S_SGC_CH{i}'] = np.sqrt(df[f'S_SG_CH{i}'] ** 2 + tsq)
-    df[f'S_SGC_DV{i}'] = np.sqrt(df[f'S_SG_DV{i}'] ** 2 + tsq)
-
-    # Sigma et Mu de chaque equipe = somme des Sigma/Mu de chaque players -ok
-    df[f'S_SGSQ_{i}'] = df[f'S_SGC_CH{i}'] ** 2 + df[f'S_SGC_DV{i}'] ** 2 
-    df[f'S_MU_{i}'] = df[f'S_MU_CH{i}'] + df[f'S_MU_DV{i}']
+def calc_oskill_bycr(df, os_sg, os_mu):
     
-# Calcul "C" = sqrt(somme pour toutes les equipes de Sigma^2 + beta^2) -ok
-bsq = params.get('beta') ** 2
-c = np.sqrt(df['S_SGSQ_1'] + df['S_SGSQ_2'] + 2 * bsq)
-print(f'c = {c}')
+    dsg = float(25/3)
+    dmu = float(25)
+    tsq = float(25.0 / 300.0) ** 2
+    for i in range(1, 3):
+        # init sigma / mu -ok
+        df[f'OS_SG_CH{i}'] = df[f'ch_nom_{i}'].astype(str).map(os_sg).fillna(dsg)
+        df[f'OS_SG_DV{i}'] = df[f'ch_driver_{i}'].astype(str).map(os_sg).fillna(dsg) 
+        df[f'OS_MU_CH{i}'] = df[f'ch_nom_{i}'].astype(str).map(os_mu).fillna(dmu)
+        df[f'OS_MU_DV{i}'] = df[f'ch_driver_{i}'].astype(str).map(os_mu).fillna(dmu) 
 
-for i in range(1, 3):
-    # Calcul "sumQ" -ok
-    df[f'S_SUMQT_{i}'] = np.exp(df[f'S_MU_{i}']/c)
-    
+        # Correct Sigma With Tau -ok
+        df[f'S_SGC_CH{i}'] = np.sqrt(df[f'OS_SG_CH{i}'] ** 2 + tsq)
+        df[f'S_SGC_DV{i}'] = np.sqrt(df[f'OS_SG_DV{i}'] ** 2 + tsq)
 
-    #Calcul "A" -ok
-    df[f'S_A_{i}'] = 1
-    df.loc[df['win'] == 0.5, f'S_A_{i}'] = 2
+        # Sigma et Mu de chaque equipe = somme des Sigma/Mu de chaque players -ok
+        df[f'S_SGSQ_{i}'] = df[f'S_SGC_CH{i}'] ** 2 + df[f'S_SGC_DV{i}'] ** 2 
+        df[f'S_MU_{i}'] = df[f'OS_MU_CH{i}'] + df[f'OS_MU_DV{i}']
+  
+    # Calcul "C" = sqrt(somme pour toutes les equipes de Sigma^2 + beta^2) -ok
+    bsq = float(25.0 / 6.0) ** 2
+    c = np.sqrt(df['S_SGSQ_1'] + df['S_SGSQ_2'] + 2 * bsq)
 
-df[f'S_SUMQ_1'] = df[f'S_SUMQT_1'] + np.where(df['win'] >= 0.5, df[f'S_SUMQT_1'], 0)
-df[f'S_SUMQ_2'] = df[f'S_SUMQT_2'] + np.where(df['win'] <= 0.5, df[f'S_SUMQT_2'], 0)
+    for i in range(1, 3):
+        # Calcul "sumQ" -ok
+        df[f'S_SUMQT_{i}'] = np.exp(df[f'S_MU_{i}']/c)
 
-#i = 1; q = 1
-df['S_DELTA_11'] =  df['S_SUMQT_1'] / df['S_SUMQ_1'] * (1 - df['S_SUMQT_1'] / df['S_SUMQ_1']) / df['S_A_1']
-df['S_DELTA_12'] =  df['S_SUMQT_1'] / df['S_SUMQ_2'] * (1 - df['S_SUMQT_1'] / df['S_SUMQ_2']) / df['S_A_2']
-df['S_DELTA_12'] = np.where(df['win'] <= 0.5, df['S_DELTA_12'], 0)
-df['S_DELTA_1'] = (np.sqrt(df['S_SGSQ_1']) / c) * (df['S_DELTA_11'] + df['S_DELTA_12']) * df['S_SGSQ_1'] / c ** 2
+        #Calcul "A" -ok
+        df[f'S_A_{i}'] = 1
+        df.loc[df['win'] == 0.5, f'S_A_{i}'] = 2
 
-df['S_OMG_11'] = (1 - df['S_SUMQT_1'] / df['S_SUMQ_1']) / df['S_A_1']
-df['S_OMG_12'] = (df['S_SUMQT_1'] / df['S_SUMQ_2']) / df['S_A_2']
-df['S_OMG_12'] = np.where(df['win'] <= 0.5, df['S_OMG_12'], 0)
-df['S_OMG_1'] = (df['S_OMG_11'] - df['S_OMG_12']) * df['S_SGSQ_1'] / c
+    df[f'S_SUMQ_1'] = df[f'S_SUMQT_1'] + np.where(df['win'] >= 0.5, df[f'S_SUMQT_1'], 0)
+    df[f'S_SUMQ_2'] = df[f'S_SUMQT_2'] + np.where(df['win'] <= 0.5, df[f'S_SUMQT_2'], 0)
 
-df['S_DELTA_22'] =  df['S_SUMQT_2'] / df['S_SUMQ_2'] * (1 - df['S_SUMQT_2'] / df['S_SUMQ_2']) / df['S_A_2']
-df['S_DELTA_21'] =  df['S_SUMQT_2'] / df['S_SUMQ_1'] * (1 - df['S_SUMQT_2'] / df['S_SUMQ_1']) / df['S_A_1']
-df['S_DELTA_21'] = np.where(df['win'] >= 0.5, df['S_DELTA_21'], 0)
-df['S_DELTA_2'] = (np.sqrt(df['S_SGSQ_2']) / c) * (df['S_DELTA_21'] + df['S_DELTA_22']) * df['S_SGSQ_2'] / c ** 2
-df['S_OMG_22'] = (1 - df['S_SUMQT_2'] / df['S_SUMQ_2']) / df['S_A_2']
-df['S_OMG_21'] = (df['S_SUMQT_2'] / df['S_SUMQ_1']) / df['S_A_1']
-df['S_OMG_21'] = np.where(df['win'] >= 0.5, df['S_OMG_21'], 0)
-df['S_OMG_2'] = (df['S_OMG_22'] - df['S_OMG_21']) * df['S_SGSQ_2'] / c
+    #i = 1; q = 1
+    df['S_DELTA_11'] =  df['S_SUMQT_1'] / df['S_SUMQ_1'] * (1 - df['S_SUMQT_1'] / df['S_SUMQ_1']) / df['S_A_1']
+    df['S_DELTA_12'] =  df['S_SUMQT_1'] / df['S_SUMQ_2'] * (1 - df['S_SUMQT_1'] / df['S_SUMQ_2']) / df['S_A_2']
+    df['S_DELTA_12'] = np.where(df['win'] <= 0.5, df['S_DELTA_12'], 0)
+    df['S_DELTA_1'] = (np.sqrt(df['S_SGSQ_1']) / c) * (df['S_DELTA_11'] + df['S_DELTA_12']) * df['S_SGSQ_1'] / c ** 2
 
-df['S_N_MU_CH1'] = df['S_MU_CH1'] + (df['S_OMG_1'] * df['S_SGC_CH1'] ** 2 / df['S_SGSQ_1'])
-df['S_N_MU_DV1'] = df['S_MU_DV1'] + (df['S_OMG_1'] * df['S_SGC_DV1'] ** 2 / df['S_SGSQ_1'])
-df['S_N_SG_CH1'] = df['S_SGC_DV1'] * np.sqrt(np.maximum(1 - (df['S_DELTA_1'] * df['S_SGC_CH1']**2 / df['S_SGSQ_1']), 0.01))
-df['S_N_SG_DV1'] = df['S_SGC_DV1'] * np.sqrt(np.maximum(1 - (df['S_DELTA_1'] * df['S_SGC_DV1']**2 / df['S_SGSQ_1']), 0.01))
+    df['S_OMG_11'] = (1 - df['S_SUMQT_1'] / df['S_SUMQ_1']) / df['S_A_1']
+    df['S_OMG_12'] = (df['S_SUMQT_1'] / df['S_SUMQ_2']) / df['S_A_2']
+    df['S_OMG_12'] = np.where(df['win'] <= 0.5, df['S_OMG_12'], 0)
+    df['S_OMG_1'] = (df['S_OMG_11'] - df['S_OMG_12']) * df['S_SGSQ_1'] / c
 
 
-print(df[['S_SUMQT_1','S_SUMQ_1','S_DELTA_11', 'S_DELTA_12','S_SUMQT_2','S_SUMQ_2','S_DELTA_21', 'S_DELTA_22']].head())
-print(df[['S_OMG_1','S_OMG_2','S_DELTA_1','S_DELTA_2']].tail())
-print(df.tail())
+    df['S_N_MU_CH1'] = df['OS_MU_CH1'] + (df['S_OMG_1'] * df['S_SGC_CH1'] ** 2 / df['S_SGSQ_1'])
+    df['S_N_MU_DV1'] = df['OS_MU_DV1'] + (df['S_OMG_1'] * df['S_SGC_DV1'] ** 2 / df['S_SGSQ_1'])
+    df['S_N_SG_CH1'] = df['S_SGC_CH1'] * np.sqrt(np.maximum(1 - (df['S_DELTA_1'] * df['S_SGC_CH1']**2 / df['S_SGSQ_1']), 0.01))
+    df['S_N_SG_DV1'] = df['S_SGC_DV1'] * np.sqrt(np.maximum(1 - (df['S_DELTA_1'] * df['S_SGC_DV1']**2 / df['S_SGSQ_1']), 0.01))
+
+    os_sg.update(df.groupby(['ch_nom_1'])['S_N_SG_CH1'].agg("mean").to_dict())
+    os_mu.update(df.groupby(['ch_nom_1'])['S_N_MU_CH1'].agg("mean").to_dict())
+
+    to_drop = [c for c in df.columns if c.startswith('S_')]
+    df.drop(to_drop, axis=1, inplace=True)
+    return df.reset_index(drop=True)
+
+def calc_oskill(df):
+  os_sg = {}
+  os_mu = {}
+  df['win'] = (np.sign(df.ch_ordreArrivee_2 - df.ch_ordreArrivee_1) + 1.0) / 2.0
+  bycr = df.groupby('aid_cr')
+  r = bycr.apply(lambda x: calc_oskill_bycr(x,os_sg,os_mu))
+  return r
+
+
